3dc26a3d12144c1027737b9674077f11d1adbde2
Urist-McDev <102614832+Urist-McDev@users.noreply.github.com>
2023-07-12 16:47:33 -0400

    Basic JS submission

--- Basic JavaScript Projects/Project1_expressions_alert/JS/main.js
+++ Basic JavaScript Projects/Project1_expressions_alert/JS/main.js
@@ -1,3 +1,6 @@
-var A = "She said \"She sells sea shells by the sea shore\"";
-var B = " and he found that odd.";
-document.write(A + B);
+//Var makes a variable with the name and content listed
+var Sent1 = "This is the beginning of the string"
+var Sent2 = " and this is the end of the string"
+var A = "Hello World!"
+window.alert(A); //Makes a pop up window with var A.
+document.write(Sent1 + Sent2); //Writes Sent1 and Sent2 to the screen.
