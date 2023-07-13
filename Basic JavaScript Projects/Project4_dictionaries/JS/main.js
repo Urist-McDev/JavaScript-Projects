c4a2ec4363bc74e2b6b2ca27168d503253a519fc
Urist-McDev <102614832+Urist-McDev@users.noreply.github.com>
2023-07-13 12:57:52 -0400

    JS Comments

--- Basic JavaScript Projects/Project4_dictionaries/JS/main.js
+++ Basic JavaScript Projects/Project4_dictionaries/JS/main.js
@@ -1,3 +1,4 @@
+//This makes the dictionary
 var space_marine = {
     Chapter: "Salamander",
     Company: "1st",
@@ -5,11 +6,11 @@ var space_marine = {
     Age: "170"
 };
 
-function getWeapon() {
+function getWeapon() { //Deletes the value of the Weapon key, then tries to disply it. Undefined will be displayed instead
     delete space_marine.Weapon;
     document.getElementById("dictionary").innerHTML = space_marine.Weapon;
 }
 
-function getChapter() {
+function getChapter() { //Gets and displys the value of the Chapter key
     document.getElementById("chapter").innerHTML = space_marine.Chapter;
 }
