deaa41d5c1526694349ddfdff2955db1b5857d57
Urist-McDev <102614832+Urist-McDev@users.noreply.github.com>
2023-07-13 09:24:06 -0400

    Concatenating sentences

--- Basic JavaScript Projects/Project2_functions/JS/main.js
+++ Basic JavaScript Projects/Project2_functions/JS/main.js
@@ -1,4 +1,12 @@
+//This function changes a button to say the var when clicked
 function myFirstFunction() {
     var str = "This was made by a function";
     document.getElementById("Button").innerHTML = str;
+}
+
+//This function concatenates a sentence when a paragrah is clicked, then changes it to the new sentence
+function concatenateFunction() {
+    var sentence = "Functions make using JS";
+    sentence += " easier and funner!";
+    document.getElementById("Concatenate").innerHTML = sentence;
 }
