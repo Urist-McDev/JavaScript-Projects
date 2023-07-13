5aaebc71ab312c28fbe25ca31b87777df9b60dd0
Urist-McDev <102614832+Urist-McDev@users.noreply.github.com>
2023-07-13 09:10:35 -0400

    JS button

--- Basic JavaScript Projects/Project1_expressions_alert/JS/main.js
+++ Basic JavaScript Projects/Project1_expressions_alert/JS/main.js
@@ -1,6 +1,8 @@
 //Var makes a variable with the name and content listed
 var Sent1 = "This is the beginning of the string"
 var Sent2 = " and this is the end of the string"
-var A = "Hello World!"
-window.alert(A); //Makes a pop up window with var A.
+var A = "Hello World!";
 document.write(Sent1 + Sent2); //Writes Sent1 and Sent2 to the screen.
+function popUpWindow() {
+    window.alert(A);
+}
