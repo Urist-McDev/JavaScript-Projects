ea97a91a301e26c4bb7cea3f76ea0c82a16acbc6
Urist-McDev <102614832+Urist-McDev@users.noreply.github.com>
2023-07-20 09:43:45 -0400

    Finial TicTacToe game

--- Basic JavaScript Projects/TicTacToe/js/tictactoe.js
+++ Basic JavaScript Projects/TicTacToe/js/tictactoe.js
@@ -1,7 +1,7 @@
-let activePlayer = 'X';
+let activePlayer = 'X'; //This determins if the player is x or o
 let selectedSquares = [];
 
-function placeXOrO(squareNumber) {
+function placeXOrO(squareNumber) { //Sets the images to be used and places an x or o on the clicked square before switching to the computer.
     if (!selectedSquares.some(element => element.includes(squareNumber))) {
         let select = document.getElementById(squareNumber);
 
@@ -32,7 +32,7 @@ function placeXOrO(squareNumber) {
 
 }
 
-function computersTurn() {
+function computersTurn() { //How the computer decides which square to place on.
     let success = false;
     let pickASquare;
 
@@ -45,7 +45,7 @@ function computersTurn() {
     }
 }
 
-function checkWinConditions() {
+function checkWinConditions() { //Checks every possible combination to see when someone wins or is there is a tie
     if (arrayIncludes('0X', '1X', '2X')) { drawWinLine(50, 100, 558, 100) }
     else if (arrayIncludes('3X', '4X', '5X')) { drawWinLine(50, 304, 558, 304) }
     else if (arrayIncludes('6X', '7X', '8X')) { drawWinLine(50, 508, 558, 508) }
@@ -67,7 +67,7 @@ function checkWinConditions() {
         setTimeout(function () { resetGame(); }, 500);
     }
 
-    function arrayIncludes(squareA, squareB, squareC) {
+    function arrayIncludes(squareA, squareB, squareC) { //Once there is a win this will return true
         const a = selectedSquares.includes(squareA);
         const b = selectedSquares.includes(squareB);
         const c = selectedSquares.includes(squareC);
@@ -76,12 +76,60 @@ function checkWinConditions() {
     }
 }
 
-function disableClick() {
+function disableClick() { //Turns off the click for 1 second when called
     body.style.pointerEvents = 'none';
     setTimeout(function () { body.style.pointerEvents = 'auto'; }, 1000);
 }
 
-function audio(audioURL) {
+function audio(audioURL) { //Plays given audio
     let audio = new Audio(audioURL);
     audio.play();
+}
+
+function drawWinLine(coordX1, coordY1, coordX2, coordY2) { //Draws the win line
+    const canvas = document.getElementById('win-lines');
+    const c = canvas.getContext('2d');
+    let x1 = coordX1, y1 = coordY1, x2 = coordX2, y2 = coordY2, x = x1, y = y1;
+
+    function animateLineDrawing() { //Animates the line being drawn
+        const animationLoop = requestAnimationFrame(animateLineDrawing);
+
+        c.clearRect(0, 0, 608, 608);
+        c.moveTo(x1, y1);
+        c.lineTo(x, y);
+        c.lineWidth = 10;
+        c.strokeStyle = 'rgba(70, 255, 33, 0.8)';
+        c.stroke();
+
+        if (x1 <= x2 && y1 <= y2) {
+            if (x < x2) { x += 10; }
+            if (y < y2) { y += 10; }
+            if (x >= x2 && y >= y2) { cancelAnimationFrame(animationLoop); }
+        }
+
+        if (x1 <= x2 && y1 >= y2) {
+            if (x < x2) { x += 10; }
+            if (y > y2) { y -= 10; }
+            if (x >= x2 && y <= y2) { cancelAnimationFrame(animationLoop); }
+        }
+    }
+
+    function clear() { //Clears the line
+        const animationLoop = requestAnimationFrame(clear);
+        c.clearRect(0, 0, 608, 608);
+        cancelAnimationFrame(animationLoop);
+    }
+    //This block is for reseting the game on a win
+    disableClick();
+    audio('./media/winGame.mp3');
+    animateLineDrawing();
+    setTimeout(function () { clear(); resetGame(); }, 1000);
+}
+
+function resetGame() { //Clears the board for a new game
+    for (let i = 0; i < 9; i++) {
+        let square = document.getElementById(String(i));
+        square.style.backgroundImage = '';
+    }
+    selectedSquares = [];
 }
