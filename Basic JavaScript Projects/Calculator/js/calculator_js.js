504fe32329aea192d021a6f825efbf887cd47510
Urist-McDev <102614832+Urist-McDev@users.noreply.github.com>
2023-07-20 11:32:25 -0400

    Calculator

---
+++ Basic JavaScript Projects/Calculator/js/calculator_js.js
@@ -0,0 +1,91 @@
+const Calculator = { //Makes the calculator object
+    Display_Value: '0',
+    First_Operand: null,
+    Wait_Second_Operand: false,
+    operator: null,
+};
+
+function Input_Digit(digit) { //Takes the given digit and makes sure it will be displayed to the screen
+    const { Display_Value, Wait_Second_Operand} = Calculator;
+    
+    if (Wait_Second_Operand === true) {
+        Calculator.Display_Value = digit;
+    } else {
+        Calculator.Display_Value = Display_Value === '0' ? digit : Display_Value + digit;
+    }
+}
+
+function Input_Decimal(dot) { //Same as above, but for decimals
+    if (Calculator.Wait_Second_Operand === true) return;
+    if (!Calculator.Display_Value.includes(dot)) {
+        Calculator.Display_Value += dot;
+    }
+}
+
+function Handle_Operator(Next_Operator) { //Handles the operators that come in, making sure they are added to the backend
+    const {First_Operand, Display_Value, operator} = Calculator;
+    const Value_of_Input = parseFloat(Display_Value);
+
+    if (operator && Calculator.Wait_Second_Operand) {
+        Calculator.operator = Next_Operator;
+        return;
+    }
+    if (First_Operand == null) {
+        Calculator.First_Operand = Value_of_Input;
+    } else if (operator) {
+        const Value_Now = First_Operand || 0;
+
+        let result = Preform_Calculation[operator](Value_Now, Value_of_Input);
+        result = Number(result).toFixed(9);
+        result = (result *1).toString();
+        Calculator.Display_Value = parseFloat(result);
+        Calculator.First_Operand = parseFloat(result);
+    }
+    Calculator.Wait_Second_Operand = true;
+    Calculator.operator = Next_Operator;
+}
+
+const Preform_Calculation = { //Preforms the operation given by Handle_Operator
+    '/': (First_Operand, Second_Operand) => First_Operand / Second_Operand,
+    '*': (First_Operand, Second_Operand) => First_Operand * Second_Operand,
+    '+': (First_Operand, Second_Operand) => First_Operand + Second_Operand,
+    '-': (First_Operand, Second_Operand) => First_Operand - Second_Operand,
+    '=': (First_Operand, Second_Operand) => Second_Operand
+};
+
+function Calculator_Reset() { //Resets the calculator when the AC button is hit
+    Calculator.Display_Value = '0';
+    Calculator.First_Operand = null;
+    Calculator.Wait_Second_Operand = false;
+    Calculator.operator = null;
+}
+
+function Update_Display() { //Updates the display with the numbers as they come in
+    const display = document.querySelector('.calculator-screen');
+    display.value = Calculator.Display_Value;
+}
+Update_Display();
+
+const keys = document.querySelector('.calculator-keys'); //Keeps track of the button presses and lets the needed function know there was a button press
+keys.addEventListener('click', (event) => {
+    const { target } = event;
+    if (!target.matches('button')) { return; }
+
+    if (target.classList.contains('operator')) {
+        Handle_Operator(target.value);
+        Update_Display();
+        return
+    }
+    if (target.classList.contains('decimal')) {
+        Input_Decimal(target.value);
+        Update_Display();
+        return;
+    }
+    if (target.classList.contains('all-clear')) {
+        Calculator_Reset();
+        Update_Display();
+        return;
+    }
+    Input_Digit(target.value);
+    Update_Display();
+})
